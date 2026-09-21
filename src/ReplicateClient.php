e518cd60e9e66b4502a05ad39ac43318e39986b9
Ben Hynes <bkhynes@users.noreply.github.com>
2026-09-21 18:59:40 +0800

    Remove deprecated curl_close() for PHP 8.5

--- src/ReplicateClient.php
+++ src/ReplicateClient.php
@@ -211,7 +211,7 @@ final class ReplicateClient
         $raw = curl_exec($ch);
         $code = (int) curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $err = curl_error($ch);
-        curl_close($ch);
+        unset($ch);
 
         if ($raw === false) {
             throw new RuntimeException('Replicate network error: ' . $err);
